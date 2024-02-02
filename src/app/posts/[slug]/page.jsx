a634ef17ec222c91983e8bc49ae530b91024a867
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-02-02 20:10:50 +0100

    v

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Comments from "@/components/comments/Comments";
 
 const getData = async (slug) => {
-  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+  const res = await fetch(`/api/posts/${slug}`, {
     cache: "no-store",
   });
 
