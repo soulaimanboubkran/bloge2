0367ea9c4739fce4470278f20e0434fcf7eb0f46
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-02-05 00:28:46 +0100

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/t-solid.svg"  sizes="any" />
       <body className={inter.className}>
         <AuthProvider>
           <ThemeContextProvider>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className={styles.container}>
       <div className={styles.info}>
         <div className={styles.logo}>
-          <Image src="/p1.jpeg" alt=" blog" width={50} height={50} />
+          <Image src="/t-solid.svg" alt=" blog" width={50} height={50} />
           <h1 className={styles.logoText}>blog</h1>
         </div>
         <p className={styles.desc}>
