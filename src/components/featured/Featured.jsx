4b12c807f5e209fe1b39c9951c9ea8b32271b7bd
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-02-05 00:41:02 +0100

    commit

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -5,14 +5,14 @@ const Featured = () => {
   return (
     <div className='relative'>
     
-      <div className='flex'>
+      <div className='flex justify-center'>
         {/* Desktop Image */}
         <div className='hidden md:block'>
           <img src="/home.png" alt="" className='w-full h-auto rounded-2xl' />
         </div>
         {/* Mobile Image (shown on screens smaller than md breakpoint) */}
         <div className='md:hidden'>
-          <img src="/mobile.png" alt="" className='w-full h-auto rounded-2xl' />
+          <img src="/mobile.png" alt="" className='w-full  h-auto rounded-2xl' />
         </div>
       </div> <h1 className={styles.title}>
         <b>Tomass Blog!</b> Try to rich more people by posting in it.
